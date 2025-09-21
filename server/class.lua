fc7d4554eeb767e13b8cf68fbd21a1416529ac86
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-21 11:46:23 +0200

    feat(server): initial adaptive card class

---
+++ server/class.lua
@@ -0,0 +1,33 @@
+Card = {}
+Card.__index = Card
+
+---@class Card
+---@field cardData table<string, string | table> 
+
+---@class AdaptiveCardSettings
+---@field version? string defaults to 1.4
+---@field body? table[]
+---@field actions? table[] 
+
+function Card:new(data)
+    local cardData = {
+        type = 'AdaptiveCard',
+        version = data.version or '1.4',
+        body = type(data.body) == 'table' and data.body or {},
+        actions = type(data.actions) == 'table' and data.actions or {},
+    }
+
+    local cardInstance = {
+        cardData = cardData
+    }
+
+    setmetatable(cardInstance, self)
+
+    return cardInstance
+end
+
+function Card:toJson()
+    return json.encode(self.cardData)
+end
+
+return Card
