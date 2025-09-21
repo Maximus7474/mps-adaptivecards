f213409721eef7c0f06c1b775e5f97d997a28968
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-21 16:26:46 +0200

    refactor(index): use container element

--- server/index.lua
+++ server/index.lua
@@ -1,5 +1,6 @@
 local Card = require 'server.classes.card'
 local CardElement = require 'server.classes.cardElement'
+local CardContainer = require 'server.classes.cardContainer'
 
 ---@param name string
 ---@param setKickReason fun(reason: string) used to set a reason message for when the event is canceled
@@ -12,30 +13,33 @@ local function playerConnectingHandler(name, setKickReason, deferrals)
     Wait(100)
 
     local card = Card:new({
-        body = {
-            CardElement.TextBlock( {
-                text = ('Hello, %s (%d)!'):format(name, tonumber(src)),
-                style = 'heading',
-            }),
-            CardElement.Image({
-                url = 'https://placehold.co/400',
-                alignment = 'Center',
-                size = 'medium',
-            }),
-            CardElement.Media({
-                poster = 'https://placehold.co/69',
-                sources = {
-                    url = 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-                    mimeType = 'video/mp4'
-                },
-            }),
-        },
         actions = {{
             type = "Action.Submit",
             title = 'This is a title',
             data = { 'this is data '}
         }}
     })
+
+    local container = CardContainer:new(nil,
+        CardElement.TextBlock( {
+            text = ('Hello, %s (%d)!'):format(name, tonumber(src)),
+            style = 'heading',
+        }),
+        CardElement.Image({
+            url = 'https://placehold.co/69',
+            alignment = 'Center',
+            size = 'medium',
+        }),
+        CardElement.Media({
+            poster = 'https://placehold.co/69',
+            sources = {
+                url = 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+                mimeType = 'video/mp4'
+            },
+        })
+    )
+
+    card:addElement(container:getComponent())
 
     deferrals.presentCard(card:toJson(), function (data, rawData)
         print(json.encode(data, {indent=true}))
