fc4e04c256bc0761a3dd33240669cad6f27fbbc0
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-21 11:47:12 +0200

    feat: fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+title 'Adaptive Cards builder'
+description 'A simple demo resource in using adaptive cards'
+author 'Maximus7474'
+version '0.0.0'
+
+server_scripts {
+    '@ox_lib/init.lua',
+    'server/index.lua',
+}
