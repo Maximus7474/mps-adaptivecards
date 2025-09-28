7404ac98065492acddb4795c542265c1a1c57050
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-28 15:36:48 +0200

    chore(index): update demo card to use the columnset class

--- server/index.lua
+++ server/index.lua
@@ -1,6 +1,8 @@
 local Card = require 'server.lib.card'
 local CardElement = require 'server.lib.cardElement'
 local CardContainer = require 'server.lib.cardContainer'
+local CardColumnClasses = require 'server.lib.cardColumnSet'
+local CardColumnSet, CardColumn = CardColumnClasses.CardColumnSet, CardColumnClasses.CardColumn
 
 ---@param name string
 ---@param setKickReason fun(reason: string) used to set a reason message for when the event is canceled
@@ -20,16 +22,38 @@ local function playerConnectingHandler(name, setKickReason, deferrals)
         }}
     })
 
-    local container = CardContainer:new(nil,
-        CardElement.TextBlock( {
-            text = ('Hello, %s (%d)!'):format(name, tonumber(src)),
-            style = 'heading',
-        }),
-        CardElement.Image({
-            url = 'https://placehold.co/69',
-            alignment = 'Center',
-            size = 'medium',
-        }),
+    local columnSet = CardColumnSet:new({
+        alignement = {
+            horizontal = "Left"
+        }
+    },
+        CardColumn:new({},
+            CardElement.Image({
+                url = 'https://placehold.co/69',
+                alignment = 'Center',
+                size = 'medium',
+            })
+        ),
+        CardColumn:new({},
+            CardElement.TextBlock( {
+                text = ('Hello, %s (%d)!'):format(name, tonumber(src)),
+                style = 'heading',
+            })
+        )
+    )
+
+    local container = CardContainer:new({
+        alignement = {
+            horizontal = 'Center',
+            vertical = 'Center'
+        },
+        spacing = 'Large',
+    },
+        columnSet:getComponent()
+    )
+
+    card:addElement(
+        container:getComponent(),
         CardElement.Media({
             poster = 'https://placehold.co/69',
             sources = {
@@ -39,10 +63,8 @@ local function playerConnectingHandler(name, setKickReason, deferrals)
         })
     )
 
-    card:addElement(container:getComponent())
-
     deferrals.presentCard(card:toJson(), function (data, rawData)
-        print(json.encode(data, {indent=true}))
+        print('adaptive card actions cb', json.encode(data, {indent=true}))
     end)
 end
 
