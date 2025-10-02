af25460d6415d1483dbeaee2857a1feb12d7d74e
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-02 19:51:38 +0200

    feat(index): commented import for the basicPassword module

--- server/index.lua
+++ server/index.lua
@@ -4,4 +4,7 @@ local handleConnecting = require 'server.demos.genericDemo'
 -- A demo for discord whitelisting, has different cards to show depending on the status
 -- local handleConnecting = require 'server.demos.whitelistExample'
 
+-- A demo for a basic server password set via convar
+-- local handleConnecting = require 'server.demos.basicPassword'
+
 AddEventHandler("playerConnecting", handleConnecting)
